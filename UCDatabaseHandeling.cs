6b9bc4490ca95b846d02a78684dfb01cb2aa001a
Bhathya Bandara <bhathiya.rb@gmail.com>
2023-06-07 15:21:36 +0530

    userAddingSetting1

--- UCDatabaseHandeling.cs
+++ UCDatabaseHandeling.cs
@@ -1,17 +1,6 @@
-﻿using Microsoft.VisualBasic.ApplicationServices;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Xml.Linq;
-
 namespace project_TelegraphicTransfer
 {
     public partial class UCDatabaseHandeling : UserControl
@@ -24,25 +13,6 @@ namespace project_TelegraphicTransfer
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void panel3_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void panel2_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
 
         private void UCDatabaseHandeling_Load(object sender, EventArgs e)
         {
@@ -137,53 +107,56 @@ namespace project_TelegraphicTransfer
             tbPW.Text = "";
             cbUL.SelectedIndex = -1;
 
-            private void InsertUserToDatabase(string name, string password, string userLevel)
-            {
-                try
-                {
-                    // Check if the name already exists in the database
-                    connsql.Open();
-                    SqlCommand checkCmd = new SqlCommand("SELECT COUNT(*) FROM tbl_LOGIN_MASTER WHERE NAME = @Name", connsql);
-                    checkCmd.Parameters.AddWithValue("@Name", name);
-                    int nameCount = (int)checkCmd.ExecuteScalar();
 
-                    if (nameCount > 0)
-                    {
-                        // Name already exists, display an error message
-                        MessageBox.Show("The name already exists in the database.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        return;
-                    }
-
-                    // Confirmation message before inserting the user
-                    DialogResult result = MessageBox.Show("Do you want to add this user?", "Confirmation", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
-
-                    if (result == DialogResult.OK)
-                    {
-                        // Insert the user into the database
-                        SqlCommand insertCmd = new SqlCommand("INSERT INTO tbl_LOGIN_MASTER (NAME, PASSWORD, USER_LEVEL) VALUES (@Name, @Password, @UserLevel);", connsql);
-                        insertCmd.Parameters.AddWithValue("@Name", name);
-                        insertCmd.Parameters.AddWithValue("@Password", password);
-                        insertCmd.Parameters.AddWithValue("@UserLevel", userLevel);
-                        insertCmd.ExecuteNonQuery();
-
-                        // Display a success message
-                        MessageBox.Show("User added successfully!", "User Added", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("An error occurred while inserting the user data: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-                finally
-                {
-                    connsql.Close();
-                }
-            }
 
         }
-    }
 
-                // Show confirmation message with OK and Cancel options
+        private void InsertUserToDatabase(string name, string password, string userLevel)
+        {
+            try
+            {
+                // Check if the name already exists in the database
+                connsql.Open();
+                SqlCommand checkCmd = new SqlCommand("SELECT COUNT(*) FROM tbl_LOGIN_MASTER WHERE NAME = @Name", connsql);
+                checkCmd.Parameters.AddWithValue("@Name", name);
+                int nameCount = (int)checkCmd.ExecuteScalar();
+
+                if (nameCount > 0)
+                {
+                    // Name already exists, display an error message
+                    MessageBox.Show("The name already exists in the database.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                // Confirmation message before inserting the user
+                DialogResult result = MessageBox.Show("Do you want to add this user?", "Confirmation", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+
+                if (result == DialogResult.OK)
+                {
+                    // Insert the user into the database
+                    SqlCommand insertCmd = new SqlCommand("INSERT INTO tbl_LOGIN_MASTER (NAME, PASSWORD, USER_LEVEL) VALUES (@Name, @Password, @UserLevel);", connsql);
+                    insertCmd.Parameters.AddWithValue("@Name", name);
+                    insertCmd.Parameters.AddWithValue("@Password", password);
+                    insertCmd.Parameters.AddWithValue("@UserLevel", userLevel);
+                    insertCmd.ExecuteNonQuery();
+
+                    // Display a success message
+                    MessageBox.Show("User added successfully!", "User Added", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while inserting the user data: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                connsql.Close();
+            }
+        }
+    }
+}
+
+// Show confirmation message with OK and Cancel options
 
 
 
