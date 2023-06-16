627031650adf583aa49bb096efc1c6891e44b61d
Bhathya Bandara <bhathiya.rb@gmail.com>
2023-06-16 14:20:10 +0530

    formCreation bha1

--- FormPrint.cs
+++ FormPrint.cs
@@ -10,8 +10,7 @@ using System.Windows.Forms;
 using PdfSharpCore;
 using PdfSharpCore.Drawing;
 using PdfSharpCore.Pdf;
-using PdfSharpCore.Pdf.IO;
-using PdfSharpCore.Pdf;
+
 
 namespace project_TelegraphicTransfer
 {
@@ -23,36 +22,52 @@ namespace project_TelegraphicTransfer
         }
 
 
+        private string _formName;
+        public string FormName
+        {
+            get
+            {
+                return _formName;
+            }
+            set
+            {
+                _formName = value;
+                lbl_formName.Text = _formName;  
+            }
+        }
+
+
+
 
         private void iconButton1_Click(object sender, EventArgs e)
         {
 
             try
             {
-                using(SaveFileDialog sfd =new SaveFileDialog()) 
+                using (SaveFileDialog sfd = new SaveFileDialog())
                 {
                     sfd.Filter = "pdf fils (*.pdf)|*.pdf"; //filter pdf .pdf extension
 
 
-                    if(sfd.ShowDialog() == DialogResult.OK )
+                    if (sfd.ShowDialog() == DialogResult.OK)
                     {
                         PdfDocument doc = new PdfDocument(); //create pdf document
                         doc.Info.Title = "TelegraphicTransfer"; //pdf tital
 
-                       
+
 
                         PdfPage page = doc.AddPage(); //add Page
                         page.Width = XUnit.FromMillimeter(210);
                         page.Height = XUnit.FromMillimeter(297);
 
-                        
+
                         XGraphics gfx = XGraphics.FromPdfPage(page);
 
                         //---TT Desing--
                         XFont fontphagrafe = new XFont("Calibri", 10); // Use a different font
                         XFont fz = new XFont("Calibri", 9);
                         XFont fzb = new XFont("Calibri", 10, XFontStyle.Bold);
-                        XFont fzsb = new XFont("Calibri", 9,XFontStyle.Bold);
+                        XFont fzsb = new XFont("Calibri", 9, XFontStyle.Bold);
                         XFontStyle fontStyle = XFontStyle.Bold | XFontStyle.Underline;
                         XFont mainPara = new XFont("Calibri", 14, fontStyle);
                         XBrush brush = XBrushes.Black;
@@ -61,18 +76,18 @@ namespace project_TelegraphicTransfer
                         //BOC LOGO and head
                         XImage image = XImage.FromFile(@"C:\Users\HP\source\repos\TTProject_pase1\resourses\boc.png");
                         gfx.DrawImage(image, 20, 10, 80, 80);
-                        gfx.DrawString("Traval and Remittance",fontphagrafe,XBrushes.Black,new XRect(20,75,0,0),XStringFormats.TopLeft);
+                        gfx.DrawString("Traval and Remittance", fontphagrafe, XBrushes.Black, new XRect(20, 75, 0, 0), XStringFormats.TopLeft);
                         gfx.DrawString("1st Floor,Bank of Ceylone,", fontphagrafe, XBrushes.Black, new XRect(20, 85, 0, 0), XStringFormats.TopLeft);
                         gfx.DrawString("Head office, Colombo 01", fontphagrafe, XBrushes.Black, new XRect(20, 95, 0, 0), XStringFormats.TopLeft);
                         gfx.DrawString("phone", fontphagrafe, XBrushes.Black, new XRect(20, 105, 0, 0), XStringFormats.TopLeft);
                         gfx.DrawString("011-2445783", fontphagrafe, XBrushes.Black, new XRect(60, 105, 0, 0), XStringFormats.TopLeft);
-                        gfx.DrawString("011-2203173",fontphagrafe, XBrushes.Black, new XRect(60, 117, 0, 0), XStringFormats.TopLeft);
-                        gfx.DrawString("Email", fontphagrafe, XBrushes.Black, new XRect(20,130, 0, 0), XStringFormats.TopLeft);
+                        gfx.DrawString("011-2203173", fontphagrafe, XBrushes.Black, new XRect(60, 117, 0, 0), XStringFormats.TopLeft);
+                        gfx.DrawString("Email", fontphagrafe, XBrushes.Black, new XRect(20, 130, 0, 0), XStringFormats.TopLeft);
                         gfx.DrawString("travel@boc.lk", fontphagrafe, XBrushes.Black, new XRect(60, 130, 0, 0), XStringFormats.TopLeft);
                         //end logo and details
 
                         //office use only box
-                        XRect recOfficeuse = new XRect(143,37,170,100);
+                        XRect recOfficeuse = new XRect(143, 37, 170, 100);
                         XPen pen = new XPen(XColors.Black, 1);
                         gfx.DrawRectangle(pen, recOfficeuse);
 
@@ -85,8 +100,8 @@ namespace project_TelegraphicTransfer
                         XRect textBoundsOffice = new XRect(recOfficeuse.Left, 45, recOfficeuse.Width, recOfficeuse.Height);
                         gfx.DrawString(office, fzb, brush, textBoundsOffice, XStringFormats.TopCenter);
 
-                        XRect textBoundsTtref = new XRect(150, 67,0,0);
-                        gfx.DrawString(ttref, fz,brush, textBoundsTtref, XStringFormats.TopLeft);
+                        XRect textBoundsTtref = new XRect(150, 67, 0, 0);
+                        gfx.DrawString(ttref, fz, brush, textBoundsTtref, XStringFormats.TopLeft);
 
                         XRect textBoundsrf = new XRect(150, 82, 0, 0);
                         gfx.DrawString(rf, fz, brush, textBoundsrf, XStringFormats.TopLeft);
@@ -105,10 +120,10 @@ namespace project_TelegraphicTransfer
 
                         string branch = "BRANCH USE ONLY";
                         string costCB = "Cost Center  Branch*:.........../...............";
-                        string branchRef ="Branch Reference*:...............................";
+                        string branchRef = "Branch Reference*:...............................";
                         string conP = "Contact Person*:...................................";
                         string tp = "Phone No*:...........................................";
-                     
+
                         XRect textBoundsBranch = new XRect(355, 45, 0, 0);
                         gfx.DrawString(branch, fzb, brush, textBoundsBranch, XStringFormats.TopLeft);
 
@@ -138,9 +153,9 @@ namespace project_TelegraphicTransfer
                 }
 
             }
-            catch (Exception ex) 
-            { 
-             MessageBox.Show(ex.Message);   
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
         }
 
@@ -168,6 +183,15 @@ namespace project_TelegraphicTransfer
         private void button3_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void FormPrint_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // Enable all previously disabled forms
+            foreach (Form form in Application.OpenForms)
+            {
+                form.Enabled = true;
+            }
         }
     }
 }
